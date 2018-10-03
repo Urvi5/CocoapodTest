8876fde1689c64242fcbc97cfe86c482f238769e
Urvi <urvi.mali@cinco.ca>
2018-10-03 16:54:03 -0400

    commit

---
+++ CocoaPodTest/CocoaPodTest.h
@@ -0,0 +1,19 @@
+//
+//  CocoaPodTest.h
+//  CocoaPodTest
+//
+//  Created by URVI MALI on 2018-10-03.
+//  Copyright © 2018 CINCO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaPodTest.
+FOUNDATION_EXPORT double CocoaPodTestVersionNumber;
+
+//! Project version string for CocoaPodTest.
+FOUNDATION_EXPORT const unsigned char CocoaPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodTest/PublicHeader.h>
+
+
